3e0f9f35e8b61cb27b7e83c7995e6698814c018c
tungphien <tungphien.cn08@gmail.com>
2017-06-03 14:13:40 +0700

    Update source to handle set properties for textbox, combobox
    Update source to handle set properties for textbox, combobox

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@
 var controlProperty = {
     'Label': {
         'text': 'label'
+    },
+    'Textbox': {
+        'placeHolder': '',
+        'style': ''
+    },
+    'Combobox': {
+        'items': ''
     }
 };
 $(document).ready(function () {
@@ -19,10 +26,29 @@ $(document).ready(function () {
         cursor: 'move',
         revert: true
     });
+
+    $(document).on('click', '.remo_control', function (event) {
+        $(this).parents('.obj-dropped.selected').remove();
+    });
     $(document).on('keyup', '#properties input', function (event) {
-        var type = $(this).data('name');
-        if (type == 'text') {
-            $('.obj-dropped.selected').find('label:first-child').text($(this).val());
+        var attr = $(this).data('attr');
+        var control_name = $(this).data('control_name');
+        var control = $('.obj-dropped.selected').find("[name=" + control_name + "]");
+        if (attr == 'text') {
+            control.text($(this).val());
+        }
+        if (attr == 'placeHolder') {
+            control.attr(attr, $(this).val());
+        }
+        if (attr == 'style') {
+            control.attr(attr, $(this).val());
+        }
+        if (attr == 'items') {
+            var items = $(this).val().split(';');
+            control.html('');
+            items.map(function (item) {
+                control.append(`<option value="${item}">${item}</option>`);
+            });
         }
     });
     $(document).on('click', '.obj-dropped', function (event) {
@@ -31,53 +57,85 @@ $(document).ready(function () {
         $(this).addClass('selected');
         var typeControl = $(this).data('type');
         var property = $("#properties");
+        var cp = controlProperty[typeControl];
+        var control = null;
+        var ctrName = "";
         property.html("");
         switch (typeControl) {
             case 'Label':
             {
-                var cp = controlProperty[typeControl];
-                var control = $(this).find('label:first-child');
+                control = $(this).find('label:first-child');
                 var text = control.html();
                 cp['text'] = text;
-                Object.keys(cp).forEach(function (key, index) {
-                    property.append(`<div>
-                    ${key}
-                    </div>
-                    <div><input type="text" data-name="${key}" value="${cp[key]}" /></div>
-                    `);
+
+                break;
+            }
+            case 'Textbox':
+            {
+                control = $(this).find('input:first-child');
+                var ph = $(control).attr('placeHolder');
+                var st = $(control).attr('style');
+                if (typeof(ph) != "undefined")
+                    cp['placeHolder'] = ph;
+                if (typeof(st) != "undefined")
+                    cp['style'] = st;
+
+                break;
+            }
+            case 'Combobox':
+            {
+                control = $(this).find('select:first-child');
+                var items = [];
+                $(control).find('option').each(function () {
+                    //alert(this.text + ' ' + this.value);
+                    items.push(this.text);
                 });
+                cp['items'] = items.join(';');
+
                 break;
             }
             // case ...
         }
+        if (control != null) {
+            ctrName = $(control).attr('name');
+        }
+        if (typeof(cp) != "undefined")
+            Object.keys(cp).forEach(function (key, index) {
+                property.append(`<div>
+                    ${key}
+                    </div>
+                    <div><input type="text" data-attr="${key}" data-control_name="${ctrName}" value="${cp[key]}" /></div>
+                    `);
+            });
+
     });
 });
 
 function mapAndRenderControl(type) {
-
+    var date = new Date();
+    var controlName = date.getTime();
+    var closeDiv = `<a class="remo_control" title="Remove control">x</a>`;
     switch (type) {
         case 'Textbox':
-            return `<input type="text" placeHolder="Textbox"/>`;
+            return `<input name="txt_${controlName}" type="text" placeHolder="Textbox"/>${closeDiv}`;
             break;
         case 'Combobox':
-            return `<select>
-        <option value="volvo">Item 1</option>
-        <option value="saab">Item 2</option>
-        <option value="opel">Item 3</option>
-        <option value="audi">Item 4</option>
-        </select>`;
+            return `<select name="${controlName}">
+        <option value="Item 1">Item 1</option>
+        <option value="Item 2">Item 2</option>
+        </select>${closeDiv}`;
             break;
         case 'Textarea':
-            return `<textarea rows="4">content of text area</textarea>`;
+            return `<textarea name="txtarea_${controlName}" rows="4">content of text area</textarea>${closeDiv}`;
             break;
         case 'Radio':
-            return `<input type="radio"/>`;
+            return `<input name="_rdo_${controlName}" type="radio"/>${closeDiv}`;
             break;
         case 'Checkbox':
-            return `<input type="checkbox"/>`;
+            return `<input name="chk_${controlName}" type="checkbox"/>${closeDiv}`;
             break;
         case 'Label':
-            return `<label>Label</label>`;
+            return `<label name="lbl_${controlName}">Label</label>${closeDiv}`;
             break;
         default:
             return type;
